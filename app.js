525faab80fc010989400016223518723bfacdc5f
aftab.ali <@vanZA2021+#>
2024-05-27 02:20:52 +0400

    update

--- app.js
+++ app.js
@@ -263,6 +263,7 @@ app.post('/api/sf/getAccountsDetails', (req, res) => {
 
     
     if((externalID !== null && externalID !== undefined ) && externalID == "456781428"){
+        console.log('external ID 1: ' + this.externalID);
         fs.readFile('Account_details.json', 'utf8', (err, data) => {
             if (err) {
                 console.error(err);
