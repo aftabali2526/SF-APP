ac6424a7a21338ec43330f75b9809281317d7069
aftab.ali <@vanZA2021+#>
2024-05-27 15:48:17 +0400

    update

--- app.js
+++ app.js
@@ -290,7 +290,7 @@ app.post('/api/sf/getAccountsDetails', (req, res) => {
           return  res.status(200).json(JSON.parse(data));
         });
     }else if((externalID !== null && externalID !== undefined ) && externalID == "456785493"){
-        fs.readFile('CreditCard_Details.json', 'utf8', (err, data) => {
+        fs.readFile('Loan_Account_Details.json', 'utf8', (err, data) => {
             if (err) {
                 console.error(err);
                 res.status(500).json({ error: 'Internal Server Error' });
@@ -299,7 +299,7 @@ app.post('/api/sf/getAccountsDetails', (req, res) => {
           return  res.status(200).json(JSON.parse(data));
         });
     }else if((externalID !== null && externalID !== undefined ) && externalID == "456782543"){
-        fs.readFile('Tdr_Account_Details.json', 'utf8', (err, data) => {
+        fs.readFile('CreditCard_Details.json', 'utf8', (err, data) => {
             if (err) {
                 console.error(err);
                 res.status(500).json({ error: 'Internal Server Error' });
