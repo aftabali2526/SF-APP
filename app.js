f901cda94d57522751622ad2f1abc2e97161e48e
aftab.ali <@vanZA2021+#>
2024-06-07 17:51:58 +0400

    update

--- app.js
+++ app.js
@@ -484,6 +484,46 @@ app.post('/api/sf/updateCardStatus', (req, res) => {
 });
 
 
+
+app.post('/api/sf/getCards', (req, res) => {
+    // Extract user data from request body
+    const { channelid, userid, password,terminalId,messageType,dateTime,tranCode,stan } = req.headers;
+    const { cif, externalId } = req.body;
+    console.log(JSON.stringify(req.headers));
+    // Validate request data (for demonstration purposes)
+    if (!channelid || !userid || !password) {
+        return res.status(400).json({ error: 'Missing header fields' });
+    }
+
+    if (!cif) {
+        return res.status(400).json({ error: 'Missing required fields, external id in request' });
+    }
+
+    
+    if(cif !== null && cif != undefined && cif !== '' && cif == '4567893'){
+        fs.readFile('Cards_List.json', 'utf8', (err, data) => {
+            if (err) {
+                console.error(err);
+                res.status(500).json({ error: 'Internal Server Error' });
+                return;
+            }
+            res.status(200).json(JSON.parse(data));
+        });
+    }else {
+        fs.readFile('Error.json', 'utf8', (err, data) => {
+            if (err) {
+                console.error(err);
+                res.status(500).json({ error: 'Internal Server Error' });
+                return;
+            }
+            res.status(200).json(JSON.parse(data));
+        });
+    }
+     
+});
+
+
+
 // Start the server
 app.listen(port, () => {
     console.log(`Server is running on http://localhost:${port}`);
